cd4800f04817ea861423e246a5b753c4f27e4690
dreed4 <dreed4@uccs.edu>
2018-03-29 15:20:17 -0600

    Backbone of CNN finished.
    Runs but definitely doesn't seem right

--- src/toxic_comments-cnn.py
+++ src/toxic_comments-cnn.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 from keras.preprocessing.text import Tokenizer
 from keras.preprocessing.sequence import pad_sequences
 from keras.layers import Dense, Input, LSTM, Embedding, Dropout, Activation
-from keras.layers import Bidirectional, GlobalMaxPool1D
+from keras.layers import Bidirectional, GlobalMaxPool1D, Flatten
 from keras.models import Model, Sequential
 from keras import initializers, regularizers, constraints, optimizers, layers
 from setuptools.dist import sequence
@@ -17,17 +17,28 @@ def build_model(num_words, max_vec_size, inp_len):
         This should build the model, but right now it isn't really ready to be 
         trained because I don't think the layers are correct
     '''
+    print("Building model..")
     
     model = Sequential()
     model.add(Embedding(num_words, max_vec_size, input_length=inp_len))
     #TODO: add convolutional layers
-    model.add(Conv1D(128,5, activation='relu'))
-    model.add(MaxPooling1D(5))
-    model.Flatten()
-    model.add(Dense(128, activation='relu'))
-    model.add(Dense(64, activation='relu'))
-    model.add(Dense(6, activation='sigmoid'))
+    model.add(Conv1D(64,3, activation='sigmoid'))
+    model.add(Conv1D(32,3, activation='sigmoid'))
+    model.add(Conv1D(16,3, activation='sigmoid'))
+    #I had this pooling layer but maybe not needed?
+    #model.add(MaxPooling1D(5))
+    model.add(Flatten())
+    #dropout to prevent overfitting
+    model.add(Dropout(0.2))
     
+    model.add(Dense(180, activation='sigmoid'))
+    model.add(Dropout(0.2))
+    
+    model.add(Dense(1, activation='sigmoid'))
+    model.compile(loss='binary_crossentropy', optimizer='adam', 
+    metrics=['accuracy'])
+    
+    return model
     
 def get_data(file_path):
     print("getting data..")
@@ -42,11 +53,11 @@ def get_data(file_path):
     return((train, test, data))
 
     
-def get_tokenizer(train, nwords):
+def get_tokenizer(train_comments, nwords):
     print("getting tokenizer..")
     
     t = Tokenizer(num_words=nwords)
-    texts = train["comment_text"]
+    texts = train_comments
     t.fit_on_texts(texts)
     sequences = t.texts_to_sequences(texts)
     
@@ -56,7 +67,7 @@ def pad_data(sequences,maxln):
     print("padding data..")
     
     #we could set a max_len here but chose not to, let it default to max
-    data  = pad_sequences(sequences)
+    data  = pad_sequences(sequences, maxlen = maxln)
     return data
 
 def main():
@@ -72,35 +83,105 @@ def main():
     print("Sets should be split into test and train now..")
     print("Train size: ", len(train))
     print("Test size: ", len(test))
-    print("############## Null testing ############\n", train.isnull().any(),test.isnull().any())
+    print("############## Testing for null vals ############\n", 
+    all_data.isnull().any(),test.isnull().any())
     
-    classes_lst = ["toxic","severe_toxic","obscene","threat","insult","identity_hate"]
+    classes_lst = ["toxic","severe_toxic","obscene","threat","insult",
+    "identity_hate"]
+    
+    
     
     #for now I'll leave all the dependent values in their own arrays
-    y_toxic = train["toxic"].values
-    y_severe = train["severe_toxic"].values
-    y_obscene = train["obscene"].values
-    y_threat = train["threat"].values
-    y_insult = train["insult"].values
-    y_identity = train["identity_hate"].values
+    y_train_toxic = train["toxic"].values
+    y_train_severe = train["severe_toxic"].values
+    y_train_obscene = train["obscene"].values
+    y_train_threat = train["threat"].values
+    y_train_insult = train["insult"].values
+    y_train_identity = train["identity_hate"].values
+    
+    #same for test set
+    y_test_toxic = test["toxic"].values
+    
+    
     
     comments_train_lst = train["comment_text"]
     comments_test_lst = test["comment_text"]
     
-    #TODO: do tokenization
-    tokenizer_tup = get_tokenizer(train, num_words)
-    t = tokenizer_tup[0]
-    sequences = tokenizer_tup[1]
+    #tokenizing and padding for training set
+    tokenizer_train_tup = get_tokenizer(comments_train_lst, num_words)
+    tokenizer_train = tokenizer_train_tup[0]
+    sequences_train = tokenizer_train_tup[1]
+    #the max is really about 1400, but just 
+    #to be safe
+    train_padded = pad_data(sequences_train, 1500)
+    print("len(train_padded[0]): ", len(train_padded[0]))
     
-    train_padded = pad_data(sequences, maxlen) 
+    #tokenizing and padding for testing set
+    tokenizer_test_tup = get_tokenizer(comments_test_lst, num_words)
+    tokenizer_test = tokenizer_test_tup[0]
+    sequences_test = tokenizer_test_tup[1]
+    test_padded = pad_data(sequences_test, 1500)
     
-    print(train_padded)
-   
+    
+     
+    
+    print("###### Padded data ######")
+    print("train_padded: ", train_padded)
+    print("\ntest_padded: ",  test_padded)
+    print("\n########################")
+    
+
+    
+    
+    
+    
+    
+    #setting x vals for test and train
+    x_train = train_padded
+    x_test = test_padded
+    
+    #y vals for test and train
+    #for now, we're just doing one column (toxic), until we know it works and we
+    #can do more
+    y_train = y_train_toxic
+    y_test = y_test_toxic
+    
+    print("Printing values that will go into fit..")
+    print("x_train: ", x_train)
+    print("Shape: (", len(x_train[0]), ",", len(x_train), ")")
+    print("\n","x_test: ", x_test)
+    
+    print("\n","y_train: ", y_train)
+    print("\n","y_test: ", y_test)
+    
+    ###### SHOULD BE DEPRECATED. TODO: REMOVE IF UNNECESSARY #######
     inp_len = len(train_padded[0])
     print("len(): ", inp_len)
-    #TODO: add all layers for network
-    model = build_model(num_words, max_vec_size, inp_len)
+    ################################################################
     
-    pass
-
+    model = build_model(num_words, max_vec_size, 1500)
+    
+    print("Fitting model..")
+    history = model.fit(x_train, y_train, validation_data=(x_test, y_test), epochs=2, 
+    batch_size=100, verbose=1)
+    
+    print(history.history.keys())
+    
+    #found the following code on machinelearningmastery
+    # summarize history for accuracy
+    plt.plot(history.history['acc'])
+    plt.plot(history.history['val_acc'])
+    plt.title('model accuracy')
+    plt.ylabel('accuracy')
+    plt.xlabel('epoch')
+    plt.legend(['train', 'test'], loc='upper left')
+    plt.show()
+    # summarize history for loss
+    plt.plot(history.history['loss'])
+    plt.plot(history.history['val_loss'])
+    plt.title('model loss')
+    plt.ylabel('loss')
+    plt.xlabel('epoch')
+    plt.legend(['train', 'test'], loc='upper left')
+    plt.show()
 main()
