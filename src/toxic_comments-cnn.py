affda0fa1a22f929fa7c404de1ebd6e9af5cfcb5
dreed4 <dreed4@uccs.edu>
2018-05-08 22:01:21 -0600

    Added ability to do multi-label, changed params for cnn

--- src/toxic_comments-cnn.py
+++ src/toxic_comments-cnn.py
@@ -9,6 +9,7 @@ from keras import initializers, regularizers, constraints, optimizers, layers
 from setuptools.dist import sequence
 from keras.layers.convolutional import Conv1D
 from sklearn.metrics import confusion_matrix
+from sympy.matrices.densetools import row
 
 #split train and test sets
 #will just create two files
@@ -16,14 +17,14 @@ from sklearn.metrics import confusion_matrix
 def build_model2(num_words, max_vec_size, inp_len):
     model = Sequential()
     model.add(Embedding(num_words, max_vec_size, input_length=inp_len)) 
-    model.add(Conv1D(128, 5, activation='relu'))
+    model.add(Conv1D(256, 5, activation='relu'))
     model.add(MaxPooling1D(5))
     model.add(Conv1D(128, 5, activation='relu'))
     model.add(MaxPooling1D(5))
     model.add(Flatten())
-    model.add(Dense(128, activation='relu'))
     model.add(Dense(64, activation='relu'))
-    model.add(Dense(1, activation='sigmoid'))
+    model.add(Dense(32, activation='relu'))
+    model.add(Dense(6, activation='sigmoid'))
     
     model.compile(loss='binary_crossentropy', optimizer='adam', 
     metrics=['accuracy'])
@@ -31,8 +32,7 @@ def build_model2(num_words, max_vec_size, inp_len):
     return model
 def build_model1(num_words, max_vec_size, inp_len):
     '''
-        This should build the model, but right now it isn't really ready to be 
-        trained because I don't think the layers are correct
+        
     '''
     print("Building model..")
     
@@ -49,7 +49,7 @@ def build_model1(num_words, max_vec_size, inp_len):
     model.add(MaxPooling1D(4))
     model.add(Conv1D(128,3, activation='relu'))
     model.add(MaxPooling1D(4))
-    model.add(Conv1D(128,3,activation='relu'))
+    model.add(Conv1D(64,3,activation='relu'))
     model.add(MaxPooling1D(4))
     model.add(Flatten())
     #dropout to prevent overfitting; originally 0.2
@@ -57,13 +57,13 @@ def build_model1(num_words, max_vec_size, inp_len):
     model.add(Dropout(0.3))
     
     #this layer was sigmoid, but changed to relu
-    model.add(Dense(128, activation='relu'))
-    model.add(Dropout(0.3))
-    
     model.add(Dense(64, activation='relu'))
     model.add(Dropout(0.3))
     
-    model.add(Dense(1, activation='sigmoid'))
+    model.add(Dense(32, activation='relu'))
+    model.add(Dropout(0.3))
+    
+    model.add(Dense(6, activation='sigmoid'))
     model.compile(loss='binary_crossentropy', optimizer='adam', 
     metrics=['accuracy'])
     
@@ -103,7 +103,7 @@ def main():
     num_words = 20000
     maxlen = 300
     max_vec_size = 128
-    num_eps=2
+    num_eps=3
     
     file_path='./data.csv'
     data_sets_tup = get_data(file_path)
@@ -120,15 +120,32 @@ def main():
     "identity_hate"]
     
     #for now I'll leave all the dependent values in their own arrays
-    y_train_toxic = train["toxic"].values
-    y_train_severe = train["severe_toxic"].values
-    y_train_obscene = train["obscene"].values
-    y_train_threat = train["threat"].values
-    y_train_insult = train["insult"].values
-    y_train_identity = train["identity_hate"].values
+    y_train_toxic = np.array(train["toxic"].values)
+    y_train_severe = np.array(train["severe_toxic"].values)
+    y_train_obscene = np.array(train["obscene"].values)
+    y_train_threat = np.array(train["threat"].values)
+    y_train_insult = np.array(train["insult"].values)
+    y_train_identity = np.array(train["identity_hate"].values)
+    
+    
+    print("shapes: ", y_train_toxic.shape, y_train_severe.shape, y_train_obscene.shape, y_train_threat.shape, y_train_insult.shape, y_train_identity.shape)
+    #stack into giant 2d array
+    y_train_column_stack =np.column_stack((y_train_toxic, y_train_severe, y_train_obscene, y_train_threat, y_train_insult, y_train_identity))
     
     #same for test set
-    y_test_toxic = test["toxic"].values
+    y_test_toxic = np.array(test["toxic"].values)
+    y_test_severe = np.array(test["severe_toxic"].values)
+    y_test_obscene = np.array(test["obscene"].values)
+    y_test_threat = np.array(test["threat"].values)
+    y_test_insult = np.array(test["insult"].values)
+    y_test_identity = np.array(test["identity_hate"].values)
+    
+    #empty list
+    weights = []
+    
+    
+    print("shapes: ", y_test_toxic.shape, y_test_severe.shape, y_test_obscene.shape, y_test_threat.shape, y_test_insult.shape, y_test_identity.shape)
+    y_test_column_stack =np.column_stack((y_test_toxic, y_test_severe, y_test_obscene, y_test_threat, y_test_insult, y_test_identity))
     
     comments_train_lst = train["comment_text"]
     comments_test_lst = test["comment_text"]
@@ -137,6 +154,8 @@ def main():
     tokenizer_train_tup = get_tokenizer(comments_train_lst, num_words)
     tokenizer_train = tokenizer_train_tup[0]
     sequences_train = tokenizer_train_tup[1]
+    
+    
     #the max is really about 1400, but just 
     #to be safe
     train_padded = pad_data(sequences_train, 1500)
@@ -160,8 +179,13 @@ def main():
     #y vals for test and train
     #for now, we're just doing one column (toxic), until we know it works and we
     #can do more
-    y_train = y_train_toxic
-    y_test = y_test_toxic
+    #y_train = y_train_toxic
+    #y_test = y_test_toxic
+    
+    #y vals for test and train need to be one-hot vectors
+    y_train = y_train_column_stack
+    y_test = y_test_column_stack
+    
     
     print("Printing values that will go into fit..")
     print("x_train: ", x_train)
@@ -182,16 +206,35 @@ def main():
     history = model.fit(x_train, y_train, validation_data=(x_test, y_test), epochs=num_eps, 
     batch_size=100, verbose=1)
     
-    print("\n",history.history.keys())
     
+    labels = ['toxic', 'severe', 'obscene', 'threat', 'insult', 'identity']
     
     #confusion matrix building
     y_pred = model.predict(x_test)
     y_pred = (y_pred > 0.5)
     
-    cm = confusion_matrix(y_test, y_pred)
-    print("Confusion Matrix:\n")
-    print(cm)
+    y_pred_transpose = y_pred.transpose()
+    
+    y_test_transpose = y_test.transpose()
+    
+    for i in range(len(y_pred_transpose)):
+        this_pred = y_pred_transpose[i]
+        this_test = y_test_transpose[i]
+        this_label = labels[i]
+        
+        this_cm = confusion_matrix(this_test, this_pred)
+        
+        tn, fp, fn, tp = this_cm.ravel()
+        
+        print(this_label, " confusion matrix: \n    ", this_cm)
+        print('true negatives = ', tn, ', false positives = ', fp, ', false negatives = ', fn, ', true positives = ', tp, '\n')
+    
+    print("\n",history.history.keys())
+    print("acc: ", history.history['acc'])
+    print("val_acc: ", history.history['val_acc'])
+    
+    print("loss: ", history.history['loss'])
+    print("val_loss: ", history.history['val_loss'])
     
     #found the following code on machinelearningmastery
     # summarize history for accuracy
